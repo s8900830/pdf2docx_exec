0f63d0be28b0cbfbab3893349c19dc7467a54caa
s8900830 <77045479+s8900830@users.noreply.github.com>
2024-04-16 00:11:43 +0800

    [20240416][mark]修正與未來規劃製作有GUI頁面及能選擇好幾個檔案

--- pdf2docx/gui.py
+++ pdf2docx/gui.py
@@ -1,6 +1,46 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd
+from tkinter import messagebox
+
+
+root = tk.Tk()
+root.title('Tkinter Open File Dialog')
+root.resizable(False, False)
+root.geometry('300x150')
 
 
 def input_secretGUI():
-    pass
+    filetypes = (
+        ('text files', '*.txt'),
+        ('All files', '*.*')
+    )
+
+    filename = fd.askopenfilename(
+        title='Open a file',
+        initialdir='/',
+        filetypes=filetypes)
+
+    messagebox.showinfo(
+        title='Selected File',
+        message=filename
+    )
+
+
+# open button
+open_button = ttk.Button(
+    root,
+    text='Open a File',
+    command=input_secretGUI
+)
+
+open_button.pack(expand=True)
+
+
+# run the application
+root.mainloop()
 
 # 是不是要搞個GUI或是自動排程的功能比較好？
+
+if __name__ == '__main__':
+    pass
